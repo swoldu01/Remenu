7c8d08cc17c6267708db1c487d2277f73ada0b8c
SimealWoldu <swoldu01@gmail.com>
2023-11-24 12:31:08 -0500

    simeal

---
+++ food-review-app-backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+mongoose.connect(process.env.MONGODB_URI, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+  
